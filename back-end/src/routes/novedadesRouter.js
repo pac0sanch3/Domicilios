4e7524c70f0c72db926f6a7bbb8f9f8f77332a04
DavSRod <davidsantiagorodriguezlosada@gmail.com>
2024-10-24 19:32:01 -0500

    api domiciliarios y novedades

---
+++ back-end/src/routes/novedadesRouter.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { registrarNovedad, listarNovedad, actualizarNovedad, eliminarNovedad } from "../controllers/novedadesController.js";
+import { verificar } from "../middleware/LoginMidleware.js";
+
+const RutaNovedad = Router();
+
+RutaNovedad.post("/", verificar, registrarNovedad);
+RutaNovedad.get("/", verificar, listarNovedad);
+RutaNovedad.put("/:id", verificar, actualizarNovedad);
+RutaNovedad.delete("/:id", verificar, eliminarNovedad);
+
+export default RutaNovedad;
