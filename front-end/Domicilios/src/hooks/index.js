3200d7d75f1e88e1b7f4c543e3bfeaa23e7064f2
kevinstivencortes <kevinstivencortesmaigual36@gmail.com>
2024-10-24 20:52:56 -0500

    login2

--- front-end/Domicilios/src/hooks/index.js
+++
@@ -1,6 +0,0 @@
-
-export { useRegistrarUsuario } from "./user/useRegistrarUsuarios";
-export { useAuth } from "./user/useAuth";
-export { useFetchUserData } from "./user/useFetchUsuarios";
-export { useFetchRoles, useRegistrarRol } from "./user/useRol";
-export { useUpdateUser } from "./user/useUpdateUser";
