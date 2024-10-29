86e5786e2aae1b7374b74cd344c80206153337d7
pac0sanch3 <noriegatrujolloj123@gmail.com>
2024-10-29 17:39:41 -0500

    menu y layouy

---
+++ NewFront-end/Domicilios/src/pages/PaneldeControl.jsx
@@ -0,0 +1,14 @@
+import Layout from "../components/template/Layout";
+const PanelDeControl = () => {
+
+
+  return (
+        <>
+        <Layout/>
+        <h1>Hola</h1>
+        </>
+
+  );
+};
+
+export default PanelDeControl;
