19b618659abfe52462ab03b1d25ffa2beb2e0d10
pac0sanch3 <noriegatrujolloj123@gmail.com>
2024-11-01 05:47:18 -0500

    cambios en el menu

--- NewFront-end/Domicilios/src/components/layout/Header.jsx
+++ NewFront-end/Domicilios/src/components/layout/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 import LogoutButton from '../navegacion/LogoutButton';
 import { FaBell } from "react-icons/fa";
 import NotificacionesBell from '../notificaciones/NotificacionesBell';
@@ -11,22 +12,16 @@ const Header = () => {
   const [notificacionesPendientes, setNotificacionesPendientes] = useState(0);
 
   useEffect(() => {
-    // Llama a listarSolicitudes inicialmente
     listarSolicitudes();
-
-    // Configura un intervalo para actualizar solicitudes cada 10 segundos (ajusta el tiempo según tus necesidades)
     const interval = setInterval(() => {
       listarSolicitudes();
     }, 10000);
-
-    // Limpia el intervalo cuando el componente se desmonte
     return () => clearInterval(interval);
   }, []);
 
   useEffect(() => {
-    // Filtra las solicitudes con estado "en_curso"
     const pendientes = solicitudes.filter(solicitud => solicitud.estado === 'en_curso');
-    setNotificacionesPendientes(pendientes.length); // Actualiza el contador de notificaciones pendientes
+    setNotificacionesPendientes(pendientes.length);
   }, [solicitudes]);
 
   const toggleNotifications = () => {
@@ -35,38 +30,50 @@ const Header = () => {
 
   return (
     <header className="inset-x-0 top-0 h-16 bg-white md:px-8 sm:px-8 max-sm:px-8 z-50">
-      <nav className="flex items-center justify-between lg:px-8 h-full" aria-label="Global"> 
-        <div className="flex lg:flex-1 items-center"> 
+      <nav className="flex items-center justify-between lg:px-8 h-full" aria-label="Global">
+        <div className="flex lg:flex-1 items-center">
           <figure className="h-full w-24 "></figure>
           <div className="flex justify-center items-center ml-3 font-bold text-2xl text-green-600 tracking-wide">
             Domicilios
           </div>
         </div>
 
-        <div className="relative flex items-center gap-6">
-<<<<<<< HEAD
-        {(userType === 'domiciliario') && (
-          <>
-                    <FaBell className="text-3xl cursor-pointer" onClick={toggleNotifications} />
-=======
-          <button className="relative" onClick={toggleNotifications}>
-            <FaBell className="text-3xl cursor-pointer" />
-            {notificacionesPendientes > 0 && (
-              <span className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-red-500 text-white text-xs font-semibold rounded-full w-5 h-5 flex items-center justify-center">
-                {notificacionesPendientes}
-              </span>
+        <div className="flex gap-6 items-center">
+          {/* Menú de navegación */}
+          <ul className="flex space-x-4">
+            <Link to="/Home" className="text-gray-800 hover:text-green-600">Home</Link>
+            <Link to="/solicitud" className="text-gray-800 hover:text-green-600">Domicilio</Link>
+            {userType === 'domiciliario' && (
+              <Link to="/novedades" className="text-gray-800 hover:text-green-600">Registrar novedad</Link>
             )}
-          </button>
->>>>>>> ed4416486442ab525f857f171f00bb2ee520ccc6
-          {showNotifications && (
-            <div className="absolute top-12 right-0 w-80 bg-white shadow-lg rounded-lg">
-              <NotificacionesBell />
-            </div>
+            {userType === 'domiciliario' && (
+              <Link to="/NotificacionesDom" className="text-gray-800 hover:text-green-600">Domicilios</Link>
+            )}
+            {userType === 'administrador' && (
+              <Link to="/PanelDeControl" className="text-gray-800 hover:text-green-600">Panel De Control</Link>
+            )}
+          </ul>
+
+          {/* Icono de notificaciones */}
+          {userType === 'domiciliario' && (
+            <>
+              <button className="relative" onClick={toggleNotifications}>
+                <FaBell className="text-3xl cursor-pointer" />
+                {notificacionesPendientes > 0 && (
+                  <span className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-red-500 text-white text-xs font-semibold rounded-full w-5 h-5 flex items-center justify-center">
+                    {notificacionesPendientes}
+                  </span>
+                )}
+              </button>
+              {showNotifications && (
+                <div className="absolute top-12 right-0 w-80 bg-white shadow-lg rounded-lg">
+                  <NotificacionesBell />
+                </div>
+              )}
+            </>
           )}
-          </>
 
-        )}
-
+          {/* Botón de logout */}
           <LogoutButton />
         </div>
       </nav>
