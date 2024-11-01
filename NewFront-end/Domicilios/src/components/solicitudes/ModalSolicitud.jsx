a37acc64339840d18d55100376d0cfccc4b2d43f
pac0sanch3 <noriegatrujolloj123@gmail.com>
2024-10-31 19:43:31 -0500

    incidentes

--- NewFront-end/Domicilios/src/components/solicitudes/ModalSolicitud.jsx
+++ NewFront-end/Domicilios/src/components/solicitudes/ModalSolicitud.jsx
@@ -20,7 +20,7 @@ const ModalSolicitud = ({ isOpen, onClose })=>{
         <div className="bg-white p-6 rounded-lg relative">
             <button
                 onClick={onClose}
-                className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
+                className="absolute top-2 right-2 bg text-gray-600 hover:text-gray-900"
             >
                 &times;
             </button>
