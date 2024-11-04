c8427391d643e0d60ce949c60b7a2293546bfb84
pac0sanch3 <noriegatrujolloj123@gmail.com>
2024-11-04 00:51:41 -0500

    borrando front-end

--- front-end/Domicilios/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { Layout } from "./template/Layout";
-export { LayotuInicio } from "./template/LayoutIn";
-
-export * from "./atoms";
-export * from "./molecules";
-export * from "./organisms";
